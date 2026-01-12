ca8aff99a7d418e67db99265f7ade997ff7c72af
Elyes MESSAADIA <elyes.messaadia@laplateforme.io>
2026-01-12 14:31:31 +0100

    Suppression des fichiers obsolètes et réorganisation du code pour améliorer la structure de l'horloge

--- main.py
+++ main.py
@@ -1,78 +1,81 @@
-import time
 import keyboard
-from clock import Clock
+import time
+from datetime import datetime
+from display import *
+from function import *
 
 def menu():
-    # On crée une instance de l'horloge
-    my_clock = Clock()
-    my_clock.start_running()
+ 
+ current_time=datetime.now()
+ set_time_alarm=None
+ time_format="24h"
+ is_paused=False
+ last_update=time.time()
 
-    while True:
-        print("\033[?25l", end="") # Cache le curseur
-        
-        # Affichage
-        print("\033[2;0H")
-        print("Clock menu")
-        print("\033[K", end="")
-        print(f"{my_clock.get_formatted_time()} ({my_clock.time_format})")
-        print("\033[4;0H")
-        print("1. Change time/tap a")
-        print("\033[5;0H")
-        print("2. Set an alarm/tap b")
-        print("\033[6;0H")
-        print("3. Reset time/tap c")
-        print("\033[7;0H")
-        print("4. change format/tap d")
-        
-        time.sleep(0.01)
-        print("\033[J", end="")
+ clear_screen()
+ while True:
 
-        # Vérification Alarme
-        if my_clock.set_time_alarm and \
-           my_clock.current_time.strftime("%H:%M:%S") == my_clock.set_time_alarm.strftime("%H:%M:%S"):
-            print("\033[H\033[J", end="")
-            print("your alarm is ringing!")
-            my_clock.set_time_alarm = None  
-            time.sleep(5)
-            continue
-        
-        # Interactions clavier
-        elif keyboard.is_pressed("a"):
-            print("\033[?25h", end="")
-            try:
-                H = int(input("choose an hour: "))
-                M = int(input("choose minutes: "))
-                S = int(input("choose seconds: "))
-                my_clock.define_time(H, M, S)
-            except ValueError:
-                print("Enter numbers only!")
-                time.sleep(2)
+  try:
 
-        elif keyboard.is_pressed("b"):
-            print("\033[?25h", end="")
-            try:
-                h_al = int(input("choose an hour: "))
-                m_al = int(input("choose minutes: "))
-                s_al = int(input("choose seconds: "))
-                alarm_str = my_clock.set_alarm(h_al, m_al, s_al)
-                print(f"alarm set at: {alarm_str}")
-                time.sleep(1)
-            except ValueError:
-                print("Enter numbers only!")
-                time.sleep(2)
+   now=time.time()
+   if now-last_update >=1:
+    current_time=running_hour(current_time, is_paused)
+    last_update=now
 
-        elif keyboard.is_pressed("c"):
-            my_clock.reset_time()
-            time.sleep(0.3)
+    
+   time_string=format_time(time_format, current_time)
+   display_menu(time_string, time_format, is_paused)
+  
+   if check_time_alarm(current_time, set_time_alarm)==True:
+      set_time_alarm=None
+      time.sleep(5)
+      clear_screen()
+      continue
+  
+   elif keyboard.is_pressed("a"):
+      show_cursor()
+      
+      H=check_hour()
+      M=check_minutes()
+      S=check_seconds()
+      clear_screen()
 
-        elif keyboard.is_pressed("d"):
-            my_clock.change_format()
-            time.sleep(0.3)
+      current_time=define_time(current_time, H,M,S)
+    
+   elif keyboard.is_pressed("b"):
+       show_cursor()
+     
+       H=check_hour()
+       M=check_minutes()
+       S=check_seconds()
+       clear_screen()
+       
+       set_time_alarm=set_alarm(H,M,S)
+       
+       print(f"Alarm set at {H:02d}:{M:02d}:{S:02d}")
 
-if __name__ == "__main__":
-    try:
-        menu()
-    except KeyboardInterrupt:
-        # On affiche un message gracieux au lieu d'une erreur technique
-        print("\n\033[?25h") # Réaffiche le curseur
-        print("Clock stopped!")
+   elif keyboard.is_pressed("c"):
+     current_time=reset_time()
+     time.sleep(0.3)
+    
+   elif keyboard.is_pressed("d"):
+     time_format=change_time(time_format)
+     time.sleep(0.3)
+  
+   elif keyboard.is_pressed("e"):
+     is_paused=paused(is_paused)
+     time.sleep(0.3)
+ 
+   time.sleep(0.1)
+
+  except KeyboardInterrupt:
+    clear_screen()
+    print("Goodbye!")
+    break 
+   
+  
+if __name__=="__main__":
+    menu()
+    
+    
+   
--- test2.py
+++
@@ -1,9 +0,0 @@
-def count_word(phrase):
-    count=1
-    for lettre in phrase:
-        if lettre == " ":
-            count+= 1
-    print(count)
-    print(phrase)
-
-count_word("Salut les gars")
